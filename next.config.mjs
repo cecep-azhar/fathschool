8ce7c9ce26d0397c2c0e79bb3ad96c87a413ef71
webfathforce <web.fathforce@gmail.com>
2024-12-21 11:04:15 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,21 +4,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: 'standalone',
-  webpack: (config) => {
-    // Menambahkan aturan untuk memuat font
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[hash].[ext]',
-          outputPath: 'static/fonts/',
-          publicPath: '/_next/static/fonts/',
-        },
-      },
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
